6f481649d9a474547d7180dff7b7069bda7a069d
mayeng20 <mariellelagare3@gmail.com>
2025-09-20 13:00:37 +0800

    Final edit for Activity5

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import React from "react";
-import ChatScreen from "./ChatScreen";
-import CommentSection from "./CommentSection";
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+import CounterApp from './CounterApp';
+import ColorChangerApp from './ColorChangerApp';
 
 export default function App() {
   return (
-    <>
-      <ChatScreen />
-      <CommentSection />
-    </>
+    <SafeAreaView style={{ flex: 1 }}>
+      <CounterApp />
+      <ColorChangerApp />
+    </SafeAreaView>
   );
 }
